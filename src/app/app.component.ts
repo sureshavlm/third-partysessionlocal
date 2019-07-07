0d3bc51084349387d9557bfcdf78b0f7bb49e710
suresh <suresh.avlm@gmail.com>
2019-07-07 10:01:06 +0530

    web storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject } from '@angular/core';
+
 import { LOCAL_STORAGE, WebStorageService } from 'angular-webstorage-service';
 
 import { SESSION_STORAGE, StorageService } from 'angular-webstorage-service';
@@ -9,15 +10,33 @@ import { SESSION_STORAGE, StorageService } from 'angular-webstorage-service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'third-party-demo';
+  
   data: any = {};
+  sessionData: any = {};
+
+  localData: any = {};
   sessionData: any = {};
 
   constructor(@Inject(SESSION_STORAGE) private sessionStorage: StorageService, @Inject(LOCAL_STORAGE) private localStorage: WebStorageService) {
   }
 
 
+
+  storeData(key, value):  void {
+    //locally
+    this.localStorage.set(key, value);
+    this.localData[key] = this.localStorage.get(key);
+    //session
+    this.sessionStorage.set(key, value);
+    this.sessionData[key] = this.sessionStorage.get(key);;
+
+  }
+
+
+/*
 
   saveInLocal(key, val): void {
 
@@ -33,5 +52,5 @@ export class AppComponent {
 
     this.data[key]= this.localStorage.get(key);
     this.sessionData[key]= this.sessionStorage.get(key);
-   }
+   }*/
 }
